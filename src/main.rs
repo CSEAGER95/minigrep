a9e8b885917971136e41d65f24ee1896a9c69818
Seager <seagerjc@gmail.com>
2026-01-02 13:51:44 -0500

    redirected error ouput to standard output.

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ fn main() {
                                                    // them in a vector called args
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments {}", err);
+        eprintln!("problem parsing arguments {}", err);
         process::exit(1);
     });
         if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
